03332e0f954b13d27e170dfb35287d9887cd2cec
xuke <xukeke0826@outlook.com>
2019-05-11 09:51:15 +0800

    没啥但是要交一下

--- text-file-process/homework.py
+++ text-file-process/homework.py
@@ -1,13 +1,9 @@
 dic = {}
-
 id_number = '学号：201811113030'
 dic[id_number] = 0
-
 with open('log_files/201811113030.log', encoding='utf8') as f:
 	for line in f:
 		if id_number in line:
 			dic[id_number] += 1
-
 print(dic)
-
 f.close()
